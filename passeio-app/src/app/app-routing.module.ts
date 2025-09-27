eb533e2b138b51344f2f3c96061ffeabe500e1fe
William Roberti Moreira <willroberti04@hotmail.com>
2025-09-27 11:18:47 -0300

    adicionando as rotas do component lugares

--- passeio-app/src/app/app-routing.module.ts
+++ passeio-app/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'paginas',
-    loadChildren: () => import('./template/template.module').then(m => m.TemplateModule)
+    loadChildren: () => import('./template/template.module').then(m => m.TemplateModule)    
   }
 ];
 
--- passeio-app/src/app/lugares/lugares-routing.module.ts
+++ passeio-app/src/app/lugares/lugares-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LugarComponent } from './lugar/lugar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LugarComponent,
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
