be6b30425ec38a28a230796456d6d61638362fb3
William Roberti Moreira <willroberti04@hotmail.com>
2025-10-01 01:04:33 -0300

    validação dos campos do form

--- passeio-app/src/app/lugares/lugar/lugar.component.ts
+++ passeio-app/src/app/lugares/lugar/lugar.component.ts
@@ -33,13 +33,23 @@ export class LugarComponent implements OnInit{
   }
 
   salvar(){
-    this.service.salvar(this.camposForm.value)
-      .subscribe({
-        next: (lugar) => {
-          console.log("Cadastrado com sucesso!", lugar);
-          this.camposForm.reset();
-        },
-        error: erro => console.error("Ocorreu um erro: ", erro)
-      });
+    this.camposForm.markAllAsTouched();
+
+    if(this.camposForm.valid){
+      this.service.salvar(this.camposForm.value)
+        .subscribe({
+          next: (lugar) => {
+            console.log("Cadastrado com sucesso!", lugar);
+            this.camposForm.reset();
+          },
+          error: erro => console.error("Ocorreu um erro: ", erro)
+        });
+    }
   }
+
+  isCampoInvalido(nomeCampo: string) : boolean{
+    const campo = this.camposForm.get(nomeCampo);  
+    return campo?.invalid && campo?.touched && campo?.errors?.['required'];
+  }    
+    
 }
