caeded78e573d9642a0c56f210f2ae26e138af25
William Roberti Moreira <willroberti04@hotmail.com>
2025-10-01 00:42:24 -0300

    implementando o service e enviando os dados para salvar na API

--- passeio-app/src/app/lugares/lugar.service.ts
+++ passeio-app/src/app/lugares/lugar.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Lugar } from './lugar';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LugarService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  salvar(lugar: Lugar) : Observable<Lugar>{
+    return this.http.post<Lugar>('http://localhost:3000/lugares', lugar);
+  }
+
+  obterTodos() : Observable<Lugar[]>{
+    return this.http.get<Lugar[]>('http://localhost:3000/lugares');
+  }
 }
--- passeio-app/src/app/lugares/lugar/lugar.component.ts
+++ passeio-app/src/app/lugares/lugar/lugar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Categoria } from '../../categorias/categoria';
 import { CategoriaService } from '../../categorias/categoria.service';
+import { LugarService } from '../lugar.service';
 
 @Component({
   selector: 'app-lugar',
@@ -13,7 +14,9 @@ export class LugarComponent implements OnInit{
   camposForm: FormGroup;
   categorias: Categoria[] = [];
 
-  constructor(private categoriaService: CategoriaService){
+  constructor(
+    private categoriaService: CategoriaService, 
+    private service: LugarService){
     this.camposForm = new FormGroup({
       nome: new FormControl('', Validators.required),
       categoria: new FormControl('', Validators.required),
@@ -30,7 +33,13 @@ export class LugarComponent implements OnInit{
   }
 
   salvar(){
-    console.log("Valores: ",this.camposForm.value);
+    this.service.salvar(this.camposForm.value)
+      .subscribe({
+        next: (lugar) => {
+          console.log("Cadastrado com sucesso!", lugar);
+          this.camposForm.reset();
+        },
+        error: erro => console.error("Ocorreu um erro: ", erro)
+      });
   }
-
 }
