a39c83e1d93abaca1957cc6958eb5aaca8fc4ef3
William Roberti Moreira <willroberti04@hotmail.com>
2025-10-04 02:14:19 -0300

    criando o modulo e componente galeria

---
+++ passeio-app/src/app/galeria/galeria/galeria.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-galeria',
+  standalone: false,
+  templateUrl: './galeria.component.html',
+  styleUrl: './galeria.component.scss'
+})
+export class GaleriaComponent {
+
+}
