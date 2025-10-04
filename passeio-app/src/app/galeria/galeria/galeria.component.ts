3a65a9f01bd755af419fd8fb61d46a44e3516b1c
William Roberti Moreira <willroberti04@hotmail.com>
2025-10-04 11:12:41 -0300

    implementação do layout da tela galeria e estrelas de avaliação

--- passeio-app/src/app/galeria/galeria/galeria.component.ts
+++ passeio-app/src/app/galeria/galeria/galeria.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Lugar } from '../../lugares/lugar';
+import { Categoria } from '../../categorias/categoria';
+import { LugarService } from '../../lugares/lugar.service';
+import { CategoriaService } from '../../categorias/categoria.service';
 
 @Component({
   selector: 'app-galeria',
@@ -6,6 +10,22 @@ import { Component } from '@angular/core';
   templateUrl: './galeria.component.html',
   styleUrl: './galeria.component.scss'
 })
-export class GaleriaComponent {
+export class GaleriaComponent implements OnInit {
 
+  lugares: Lugar[] = [];
+  categoriasFiltro: Categoria[] = [];
+
+  constructor(
+    private lugarService: LugarService,
+    private categoriaService: CategoriaService
+  ){}
+
+  ngOnInit(): void {
+      this.categoriaService.obterTodas().subscribe(categorias => this.categoriasFiltro = categorias);
+      this.lugarService.obterTodos().subscribe(lugarResposta => this.lugares = lugarResposta);
+  }
+
+  getTotalEstrelas(lugar: Lugar) : string{
+    return '&#9733;'.repeat(lugar.avaliacao || 0) + '&#9734;'.repeat(5 - (lugar.avaliacao || 0));
+  }
 }
