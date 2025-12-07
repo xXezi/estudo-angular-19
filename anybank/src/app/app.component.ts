cc4f8b77163ba12f393202da0bd816b2769aaf53
xXezi <willroberti04@hotmail.com>
2025-12-07 14:38:04 -0300

    adicionando o formsModule no projeto

--- anybank/src/app/app.component.ts
+++ anybank/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from "./banner/banner.component";
+import { FormNovaTransacaoComponent } from "./form-nova-transacao/form-nova-transacao.component";
 
 @Component({
   selector: 'app-root',
-  imports: [BannerComponent],
+  imports: [
+    BannerComponent, 
+    FormNovaTransacaoComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
