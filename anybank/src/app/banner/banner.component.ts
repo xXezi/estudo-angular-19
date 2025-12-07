7d276926bb31fced9f839a56d59d292ff10fec38
xXezi <willroberti04@hotmail.com>
2025-12-07 02:34:30 -0300

    criacao dos componentes conta e boas-vindas

--- anybank/src/app/banner/banner.component.ts
+++ anybank/src/app/banner/banner.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { BoasVindasComponent } from "./boas-vindas/boas-vindas.component";
+import { ContaComponent } from "./conta/conta.component";
 
 @Component({
   selector: 'app-banner',
-  imports: [],
+  imports: [
+    BoasVindasComponent, 
+    ContaComponent
+  ],
   templateUrl: './banner.component.html',
   styleUrl: './banner.component.css'
 })
