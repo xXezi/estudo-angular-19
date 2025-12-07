993e5c47653dfa9a86cbbaa17f5e0a761d75c780
xXezi <willroberti04@hotmail.com>
2025-12-07 01:53:58 -0300

    criando projeto anybank

---
+++ anybank/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { BannerComponent } from "./banner/banner.component";
+
+@Component({
+  selector: 'app-root',
+  imports: [BannerComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'anybank';
+}
