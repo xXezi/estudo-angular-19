231b05482e0b80579a2e0dd1b0b9ec7e4cbea956
xXezi <willroberti04@hotmail.com>
2025-12-06 23:44:24 -0300

    criado o componente livro e adicionado a lógica de favoritar

--- organo/src/app/app.component.ts
+++ organo/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { CabecalhoComponent } from "./componentes/cabecalho/cabecalho.component";
 import { RodapeComponent } from "./componentes/rodape/rodape.component";
+import { LivroComponent } from "./componentes/livro/livro.component";
 
 @Component({
   selector: 'app-root',
-  imports: [CabecalhoComponent, RodapeComponent],
+  imports: [
+    CabecalhoComponent, 
+    RodapeComponent, 
+    LivroComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
